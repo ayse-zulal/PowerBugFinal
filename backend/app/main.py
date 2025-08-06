45f8d0f1821a052285e661977831c85544acf94a
Ayşe Zülal Yıldız <165204006+ayse-zulal@users.noreply.github.com>
2025-08-06 20:03:02 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from . import models, database
 from .routes import auth, teach
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
+    "https://your-frontend-url.com",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=database.engine)  
 
