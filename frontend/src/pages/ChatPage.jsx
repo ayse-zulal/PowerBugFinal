364f27f42a205f0575e5c06fa1854357b9259454
Fatma Zehra AYTAŞ <f.zehra6938@gmail.com>
2025-08-07 23:55:53 +0300

    Update ChatPage.jsx

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -229,15 +229,7 @@ function ChatPage() {
                     </button>
                   )}
                   
-                  {!isRecording ? (
-                    <button onClick={startRecording} className="control-button record-button" title="Kaydı Başlat">
-                      <FaVideo  size={20} /> 
-                    </button>
-                  ) : (
-                    <button onClick={stopRecording} className="control-button record-button stop" title="Kaydı Durdur">
-                      <FaStop size={20} />
-                    </button>
-                  )}
+                 
 
                 <button onClick={startRecording} className="control-button record-button" title="Kaydı Başlat (TEST)">
       <FaVideo  size={20} /> 
@@ -257,6 +249,7 @@ function ChatPage() {
 
 
 export default ChatPage;
+
 
 
 
